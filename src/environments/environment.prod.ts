75a922632168bcd41352ae0cec4334c06d34bdd8
suema0331 <42561234+suema0331@users.noreply.github.com>
2021-03-21 09:05:39 +0900

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAey2oVWLPzwybKiLDPAWKNDdRpZSgTrB4",
+    authDomain: "my-pokemon-cac0b.firebaseapp.com",
+    projectId: "my-pokemon-cac0b",
+    storageBucket: "my-pokemon-cac0b.appspot.com",
+    messagingSenderId: "429145244933",
+    appId: "1:429145244933:web:c2965f5cdf2138674e636c"
+  }
+};
